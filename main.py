455b5735de959b13971eb37dc86cb182defad751
Galeano Folle Domingues <88506164+TakatJJ@users.noreply.github.com>
2022-10-27 13:18:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,11 @@ def POKEMON (pokemon_name):
     response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}/')
     print(response.status_code)
     dado_poke = response.json()
-    print(dado_poke['sprites'] ['front_shiny'], "Essa é a sprite do shiny" ) #me manda o link do sprite de um pokemonzin padrinho,
+    print(dado_poke['sprites'] ['front_shiny'], "Essa é a sprite do shiny" ) #me manda o link do sprite de um pokemonzin
 def site (url): #pega o texto de um site que eu colocar
     response = requests.get(url)
     texto = response.text #pega o texto do site
     return texto
 if __name__ == '__main__':
-    # CEP(91020001)
-    POKEMON(pokemon_name= input("Qual o nome do Pokemon que você deseja? "))
-    # response = site('https://goyabu.com/')
-    # print(response)
+   
+    POKEMON(pokemon_name= input("Qual o nome do Pokemon que você deseja? ")
